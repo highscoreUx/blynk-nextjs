8c9ef5fc08067890a7da8efa8586e053807b3459
highscoreux <boyesiji@gmail.com>
2024-08-22 20:12:51 +0100

    feat: add QR code creation

--- app/(pages)/user-home/qrcodes/create/InputComponent.tsx
+++ app/(pages)/user-home/qrcodes/create/InputComponent.tsx
@@ -4,12 +4,48 @@ import React, { useState } from "react";
 import { createLink } from "./actions";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
+import QRCode from "react-qr-code";
+
+interface QRCodeProps {
+	bgColor:
+		| "#FFFFFF"
+		| "#DE3121"
+		| "#EF8000"
+		| "#198639"
+		| "#229CE0"
+		| "#2A5BD7"
+		| "#6B52D1"
+		| "#D84280"
+		| "#000000"
+		| "transparent";
+	fgColor:
+		| "#FFFFFF"
+		| "#DE3121"
+		| "#EF8000"
+		| "#198639"
+		| "#229CE0"
+		| "#2A5BD7"
+		| "#6B52D1"
+		| "#D84280"
+		| "#000000"
+		| "transparent";
+	level: "L" | "M" | "Q" | "H";
+	size: number;
+	value: string;
+}
 
 const InputComponent = () => {
 	const [title, setTitle] = useState("");
 	const [redirectUrl, setRedirectUrl] = useState("");
 	const [isLoading, setisLoading] = useState(false);
 	const [currentPage, setCurrentPage] = useState(1);
+	const [qrprops, setQrProps] = useState<QRCodeProps>({
+		bgColor: "transparent",
+		fgColor: "#000000",
+		level: "L",
+		size: 256,
+		value: "",
+	});
 	const router = useRouter();
 
 	const linkCOnfig = () => {
@@ -52,42 +88,220 @@ const InputComponent = () => {
 		return (
 			<div>
 				<hr className="my-8" />
-				<div className="flex justify-between items-center">
-					<div></div>
-					<div className="flex gap-2 [&>*]:p-2  [&>*]:rounded-lg">
-						<button className="text-sm bg-blue-100 text-blue-950" type="button">
-							Cancel
-						</button>
-						<button
-							className="text-sm bg-blue-700 text-white"
-							onClick={async (e) => {
-								e.preventDefault();
-								const formData = new FormData();
-								formData.append("title", title);
-								formData.append("redirectUrl", redirectUrl);
-								setisLoading(true);
-								try {
-									const { message, success } = await createLink(formData);
-									if (!success) {
-										toast.dismiss();
-										toast.error(message);
-									} else {
-										toast.dismiss();
-										toast(message);
-										setTitle("");
-										setRedirectUrl("");
-										router.push("/user-home/links");
-									}
-								} catch (error) {
-								} finally {
-									setisLoading(false);
-								}
-							}}
-							disabled={!redirectUrl || isLoading}
-							type="submit"
-						>
-							{isLoading ? "working..." : "create"}
-						</button>
+				<div className="flex">
+					<div className="flex w-full justify-between [&>*]:p-2  [&>*]:rounded-lg">
+						{currentPage === 1 ? (
+							<button
+								className="text-sm bg-blue-100 text-blue-950"
+								type="button"
+							>
+								Cancel
+							</button>
+						) : (
+							<button
+								className="text-sm bg-blue-100 text-blue-950"
+								type="button"
+								onClick={() => {
+									setCurrentPage(1);
+								}}
+							>
+								Back
+							</button>
+						)}
+						{currentPage === 1 ? (
+							<button
+								className="text-sm bg-blue-700 text-white"
+								disabled={!redirectUrl}
+								type="submit"
+								onClick={(e) => {
+									e.preventDefault();
+									setCurrentPage(2);
+								}}
+							>
+								Continue
+							</button>
+						) : (
+							<button
+								className="text-sm bg-blue-700 text-white"
+								disabled={!redirectUrl}
+								type="submit"
+							>
+								Submit
+							</button>
+						)}
+					</div>
+				</div>
+			</div>
+		);
+	};
+
+	const qRConfig = () => {
+		return (
+			<div className="flex items-start">
+				<div className="flex-1">
+					<div>
+						<h3>Patterns</h3>
+						<p className="text-sm mb-3">Choose a pattern</p>
+						<div className="flex gap-4 mt-2">
+							<QRCode
+								size={56}
+								value=""
+								level="L"
+								className={`p-2 border rounded-md ${qrprops.level === "L" && "border-blue-700"}`}
+								onClick={() => {
+									setQrProps({ ...qrprops, level: "L" });
+								}}
+							/>
+							<QRCode
+								size={56}
+								value=""
+								level="M"
+								className={`p-2 border rounded-md ${qrprops.level === "M" && "border-blue-700"}`}
+								onClick={() => {
+									setQrProps({ ...qrprops, level: "M" });
+								}}
+							/>
+							<QRCode
+								size={56}
+								value=""
+								level="Q"
+								className={`p-2 border rounded-md ${qrprops.level === "Q" && "border-blue-700"}`}
+								onClick={() => {
+									setQrProps({ ...qrprops, level: "Q" });
+								}}
+							/>
+							<QRCode
+								size={56}
+								value=""
+								level="H"
+								className={`p-2 border rounded-md ${qrprops.level === "H" && "border-blue-700"}`}
+								onClick={() => {
+									setQrProps({ ...qrprops, level: "H" });
+								}}
+							/>
+						</div>
+					</div>
+					<div className="mt-4">
+						<h3>Corners</h3>
+						<p className="text-sm mb-3">Choose a corner style</p>
+						<div className="flex gap-4 mt-2 [&>div>div]:border [&>div>div]:rounded-md">
+							<div>
+								<div className="bg-white p-2">
+									<div className="h-4 w-4 bg-white-800 p-2  border-black border-4"></div>
+								</div>
+							</div>
+
+							<div>
+								<div className="bg-white p-2">
+									<div className="h-4 w-4 bg-white-800 p-2  border-black border-4 rounded-sm"></div>
+								</div>
+							</div>
+
+							<div>
+								<div className="bg-white p-2">
+									<div className="h-4 w-4 bg-white-800 p-2  border-black border-4 rounded-md"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="mt-4">
+						<h3>Background Colours</h3>
+						<p className="text-sm mb-3">Choose a Background Colour</p>
+						<div className="flex gap-4 mt-2  [&>div>div]:rounded-full flex-wrap">
+							<div
+								className={`${qrprops.bgColor === "transparent" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "transparent" });
+								}}
+							>
+								<div
+									className={`bg-transparent h-10 w-10 rounded-full border`}
+								></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#FFFFFF" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#FFFFFF" });
+								}}
+							>
+								<div className={`bg-[#FFFFFF] h-10 w-10 rounded-full`}></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#DE3121" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#DE3121" });
+								}}
+							>
+								<div className={`bg-[#DE3121] h-10 w-10 rounded-full`}></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#EF8000" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#EF8000" });
+								}}
+							>
+								<div className={`bg-[#EF8000] h-10 w-10 rounded-full`}></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#198639" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#198639" });
+								}}
+							>
+								<div className={`bg-[#198639] h-10 w-10 rounded-full`}></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#229CE0" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#229CE0" });
+								}}
+							>
+								<div className={`bg-[#229CE0] h-10 w-10 rounded-full`}></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#2A5BD7" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#2A5BD7" });
+								}}
+							>
+								<div className={`bg-[#2A5BD7] h-10 w-10 rounded-full`}></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#6B52D1" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#6B52D1" });
+								}}
+							>
+								<div className={`bg-[#6B52D1] h-10 w-10 rounded-full`}></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#D84280" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#D84280" });
+								}}
+							>
+								<div className={`bg-[#D84280] h-10 w-10 rounded-full`}></div>
+							</div>
+							<div
+								className={`${qrprops.bgColor === "#000000" && "border-2 border-blue-500"} rounded-full`}
+								onClick={() => {
+									setQrProps({ ...qrprops, bgColor: "#000000" });
+								}}
+							>
+								<div className={`bg-[#000000] h-10 w-10 rounded-full`}></div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div>
+					{/* qr */}
+					<div className="sticky top-0">
+						<QRCode
+							value={qrprops.value}
+							size={qrprops.size}
+							level={qrprops.level}
+							bgColor={qrprops.bgColor}
+						></QRCode>
 					</div>
 				</div>
 			</div>
@@ -96,7 +310,7 @@ const InputComponent = () => {
 
 	return (
 		<div>
-			{linkCOnfig()}
+			{currentPage === 1 ? linkCOnfig() : qRConfig()}
 			{bottom()}
 		</div>
 	);
--- app/(pages)/user-home/qrcodes/create/page.tsx
+++ app/(pages)/user-home/qrcodes/create/page.tsx
@@ -3,7 +3,7 @@ import InputComponent from "./InputComponent";
 
 const page = () => {
 	return (
-		<div className="md:w-[500px] md:mx-auto">
+		<div className="md:w-[600px] md:mx-auto">
 			<div>
 				<h2>Create a QR</h2>
 				<p className="text-sm mt-2">Fill in all details to create a QR code</p>
