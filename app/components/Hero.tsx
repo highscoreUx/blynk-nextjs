787236cd6ea52b34b8134ebec07e5701f30bf918
highscoreux <boyesiji@gmail.com>
2024-08-23 22:16:44 +0100

    feat: add images

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,31 +1,36 @@
-import Link from "next/link";
-import HeroImage from "../assets/HeroImage";
+import React from "react";
 import Container from "./Container";
+import Link from "next/link";
 
 const Hero = () => {
 	return (
-		<Container className="my-10 flex items-center">
-			<div
-				className="flex flex-col gap-6 [&>*]:flex-1 md:flex-row items-center"
-				id="hero"
-			>
-				<div className="flex flex-col gap-2 items-center justify-center">
-					<h1 className="tracking-tight text-center">
-						Blynk Your Links <br /> in a Flash!
+		<div className="mt-14">
+			<Container>
+				<div className="flex flex-col items-center justify-center">
+					<h1 className="tracking-tight md:text-center md:max-w-[450px] font-extrabold">
+						Blynk Your Links in a Flash!
 					</h1>
-					<p className="text-[1.15rem] leading-relaxed mt-4">
+					<p className="text-[1.15rem] leading-relaxed mt-4 md:max-w-[50%] md:text-center">
 						Transform your lengthy URLs into sleek, shareable links with Blynk —
 						your one-stop tool for quick and effortless link shortening!
 					</p>
-					<Link
-						href={"/login"}
-						className="bg-blue-700 text-white p-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
-					>
-						Get Started For Free
-					</Link>
+					<div className="flex flex-col md:flex-row md:gap-6 [&>*]:flex-1 [&>*]:flex [&>*]:items-center [&>*]:justify-center w-full md:w-[50%] lg:w-[35%] mt-4">
+						<Link
+							href={"/signup"}
+							className="bg-blue-700 text-white p-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
+						>
+							Get Started For Free
+						</Link>
+						<Link
+							href={"/login"}
+							className="bg-blue-100 text-blue-950 p-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
+						>
+							Sign in to your account
+						</Link>
+					</div>
 				</div>
-			</div>
-		</Container>
+			</Container>
+		</div>
 	);
 };
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import HeroImage from "./assets/HeroImage";
 import Hero from "./components/Hero";
+import LandingNav from "./components/LandingNav";
 
 const page = () => {
 	return (
 		<main>
+			<LandingNav />
 			<Hero />
 		</main>
 	);
