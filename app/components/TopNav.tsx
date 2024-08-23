50e39e99614b8b299088a63d6ca68c9f266a1f6c
highscoreux <boyesiji@gmail.com>
2024-08-23 16:30:04 +0100

    feat: add mobile navbar responsiveness

--- app/components/TopNav.tsx
+++ app/components/TopNav.tsx
@@ -5,11 +5,12 @@ import Container from "./Container";
 import { links } from "../constants/links";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { IoLink, IoQrCodeOutline } from "react-icons/io5";
+import { IoLink, IoMenu, IoQrCodeOutline } from "react-icons/io5";
 
 const TopNav = () => {
 	const route = usePathname();
 	const [isVisible, setIsVisible] = useState(false);
+	const [isNavVisible, setIsNavVisible] = useState(false);
 	const router = useRouter();
 
 	useEffect(() => {
@@ -34,11 +35,11 @@ const TopNav = () => {
 		};
 	}, [isVisible, router]);
 	return (
-		<div className="bg-white p-3 border-b sticky top-0">
+		<div className="bg-white p-3 border-b sticky top-0 ">
 			<Container>
-				<div className="flex text-sm justify-between items-center">
+				<div className="flex text-sm justify-between items-center relative">
 					<div>Logo</div>
-					<div className="flex gap-4">
+					<div className="md:flex gap-4 hidden">
 						{links.map(({ path, label }, index) => {
 							const isActive = route === path;
 							return (
@@ -52,7 +53,76 @@ const TopNav = () => {
 							);
 						})}
 					</div>
-					<div className="relative">
+					{isNavVisible && (
+						<div className="md:hidden gap-4 absolute flex flex-col bg-white inset-x-0 top-12 p-4 shadow-lg rounded-md">
+							{links.map(({ path, label }, index) => {
+								const isActive = route === path;
+								return (
+									<Link
+										key={index}
+										href={path}
+										className={`${isActive && "bg-blue-100 text-blue-950"} py-1 px-2 rounded-md text-neutral-600`}
+										onClick={() => {
+											setIsNavVisible(false);
+										}}
+									>
+										{label}
+									</Link>
+								);
+							})}
+							<button
+								className="p-2 bg-blue-700 text-white rounded-lg shadow-xl shadow-blue-500/40 hover:shadow-md hover:shadow-blue-500/40 "
+								onClick={() => {
+									setIsVisible(!isVisible);
+								}}
+							>
+								Create New
+							</button>
+							{isVisible && (
+								<div className="w-[200px] flex flex-col gap-2 border shadow-md p-2 rounded absolute bg-white right-0 mt-2">
+									<Link
+										href={"/user-home/links/create"}
+										className="flex items-center text-sm justify-between p-2 rounded hover:bg-blue-100/40"
+										onClick={() => {
+											isVisible && setIsVisible(false);
+											setIsNavVisible(false);
+										}}
+									>
+										<span className="flex gap-1">
+											<IoLink size={20} /> Link
+										</span>
+										<p className="py-[0.5] w-5 border rounded flex items-center justify-center">
+											L
+										</p>
+									</Link>
+									<Link
+										href={"/user-home/qrcodes/create"}
+										className="flex items-center text-sm justify-between p-2 rounded hover:bg-blue-100/40"
+										onClick={() => {
+											isVisible && setIsVisible(false);
+											setIsNavVisible(false);
+										}}
+									>
+										<span className="flex gap-1">
+											<IoQrCodeOutline size={20} /> Qr code
+										</span>
+										<p className="py-[0.5] w-5 border rounded flex items-center justify-center">
+											Q
+										</p>
+									</Link>
+								</div>
+							)}
+						</div>
+					)}
+					<div
+						className="md:hidden cursor-pointer"
+						onClick={() => {
+							setIsNavVisible(!isNavVisible);
+						}}
+					>
+						<IoMenu size={24} />
+					</div>
+					<div className="relative hidden md:block">
 						<button
 							className="p-2 bg-blue-700 text-white rounded-lg shadow-xl shadow-blue-500/40 hover:shadow-md hover:shadow-blue-500/40 "
 							onClick={() => {
