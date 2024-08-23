59e14a6822414c57babacfb18d98239f39e31b4f
highscoreux <boyesiji@gmail.com>
2024-08-23 17:21:01 +0100

    feat: add error pages

--- app/(pages)/404/page.tsx
+++ app/(pages)/404/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-	return <div>page</div>;
+	return (
+		<div className="flex flex-col items-center justify-center h-screen gap-2">
+			<h2 className="md:w-[500px] text-center">
+				The Link You're Requesting doesn't exist or has expired
+			</h2>
+			<p className="md:w-[500px] text-center">
+				We know this not likely what you've expected, please go back to homepage
+			</p>
+			<Link href={"/"} className="p-3 bg-blue-700 text-white rounded-md mt-4">
+				Go to Homepage
+			</Link>
+		</div>
+	);
 };
 
 export default page;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const notFound = () => {
-	return <div>not-found</div>;
+	return (
+		<div className="flex flex-col items-center justify-center h-screen gap-2">
+			<h2 className="md:w-[500px] text-center">
+				We searched our link archive, but we found nothing.
+			</h2>
+			<p className="md:w-[500px] text-center">
+				We know this not likely what you've expected, please go back to homepage
+			</p>
+			<Link href={"/"} className="p-3 bg-blue-700 text-white rounded-md mt-4">
+				Go to Homepage
+			</Link>
+		</div>
+	);
 };
 
 export default notFound;
