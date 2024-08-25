8498e4fc05cf5cf80804d5ad24d476139ab08642
highscoreux <boyesiji@gmail.com>
2024-08-25 14:35:11 +0100

    fix: resolve vercel build error

--- app/(pages)/user-home/links/page.tsx
+++ app/(pages)/user-home/links/page.tsx
@@ -1,4 +1,4 @@
-import { createClient } from "@/utils/supabase/server";
+import { createClient } from "@/utils/supabase/client";
 import React from "react";
 import { FiCalendar } from "react-icons/fi";
 import LinkComponent from "./LinkComponent";
