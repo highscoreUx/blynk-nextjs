305a7aaf77e52d2cc10f4973e2fef8dee91517d9
highscoreux <boyesiji@gmail.com>
2024-08-26 18:26:35 +0100

    feat: added account status

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,32 +1,50 @@
 import React from "react";
 import Container from "./Container";
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/server";
 
-const Hero = () => {
+const Hero = async () => {
+	const supabase = createClient();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
 	return (
-		<div className="my-14">
-			<Container>
+		<div className="h-[80vh] flex flex-col items-center justify-center relative">
+			<div className="h-[80vh] z-10 bg-blue-700 absolute inset-0 mix-blend-multiply"></div>
+			<div className="h-[80vh] bg-cover-image bg-cover absolute inset-0 "></div>
+			<Container className="my-14 z-20">
 				<div className="flex flex-col items-center justify-center">
-					<h1 className="tracking-tight md:text-center md:max-w-[450px] font-extrabold">
+					<h1 className="tracking-tight md:text-center md:max-w-[450px] font-extrabold text-white">
 						Blynk Your Links in a Flash!
 					</h1>
-					<p className="text-[1.15rem] leading-relaxed mt-4 md:max-w-[50%] md:text-center">
+					<p className="text-[1.15rem] leading-relaxed mt-4 md:max-w-[50%] md:text-center text-blue-100">
 						Transform your lengthy URLs into sleek, shareable links with Blynk —
 						your one-stop tool for quick and effortless link shortening!
 					</p>
-					<div className="flex flex-col md:flex-row md:gap-6 [&>*]:flex-1 [&>*]:flex [&>*]:items-center [&>*]:justify-center w-full md:w-[50%] lg:w-[35%] mt-4">
-						<Link
-							href={"/signup"}
-							className="bg-blue-700 text-white p-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
-						>
-							Get Started For Free
-						</Link>
-						<Link
-							href={"/login"}
-							className="bg-blue-100 text-blue-950 p-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
-						>
-							Sign in to your account
-						</Link>
+					<div className="flex flex-col md:flex-row md:gap-6 [&>*]:flex-1 [&>*]:flex [&>*]:items-center [&>*]:justify-center w-full md:w-[50%] lg:w-[37%] mt-4">
+						{user ? (
+							<Link
+								href={"/user-home"}
+								className="bg-white text-blue-700 p-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
+							>
+								Go to your account
+							</Link>
+						) : (
+							<Link
+								href={"/signup"}
+								className="bg-white text-blue-700 p-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
+							>
+								Get Started For Free
+							</Link>
+						)}
+						{!user && (
+							<Link
+								href={"/login"}
+								className="bg-blue-100 text-blue-950 p-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
+							>
+								Sign in to your account
+							</Link>
+						)}
 					</div>
 				</div>
 			</Container>
--- app/components/LandingNav.tsx
+++ app/components/LandingNav.tsx
@@ -3,17 +3,56 @@ import Container from "./Container";
 import Link from "next/link";
 import logo from "@/app/assets/Logo.svg";
 import Image from "next/image";
+import { createClient } from "@/utils/supabase/server";
 
-const LandingNav = () => {
+const LandingNav = async () => {
+	const supabase = createClient();
+	const {
+		data: { user },
+	} = await supabase.auth.getUser();
+
 	return (
-		<div className="border-b p-4 sticky top-0 bg-white">
+		<div className="border-b p-4 sticky top-0 bg-white z-50">
 			<Container>
-				<Link href={"/"}>
-					<div className="flex gap-2 items-center">
-						<Image src={logo} alt="Blynk"></Image>
-						<p className="text-blue-700 font-semibold text-[24px]">Blynk</p>
+				<div className="flex items-center justify-between">
+					<div>
+						<Link href={"/"}>
+							<div className="flex gap-2 items-center">
+								<Image src={logo} alt="Blynk"></Image>
+								<p className="text-blue-700 font-semibold text-[24px]">Blynk</p>
+							</div>
+						</Link>
 					</div>
-				</Link>
+					<div>
+						{user ? (
+							<Link
+								href={"/user-home"}
+								className="bg-blue-700 text-white py-2 px-4 rounded-lg  mr-4 mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
+							>
+								My Account
+							</Link>
+						) : (
+							<Link
+								href={"/signup"}
+								className="bg-blue-700 text-white py-2 px-4 rounded-lg  mr-4 mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
+							>
+								Sign Up
+							</Link>
+						)}
+						{user ? (
+							<button className="bg-blue-100 text-blue-950 py-2 px-4 rounded-lg  text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]">
+								Logout
+							</button>
+						) : (
+							<Link
+								href={"/login"}
+								className="bg-blue-100 text-blue-950 py-2 px-4 rounded-lg mt-4 text-sm focus:outline-blue-300/30 focus:outline-2 hover:scale-[1.05]"
+							>
+								Login
+							</Link>
+						)}
+					</div>
+				</div>
 			</Container>
 		</div>
 	);
